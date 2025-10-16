e9cd56a178d56e4553aea83f1cebd755e42c454f
remiCzn <cazinremi2@gmail.com>
2025-10-16 09:03:17 +0200

    feat: setup deployment

--- build.sh
+++ build.sh
@@ -11,4 +11,7 @@ cd ../
 mkdir -p ./dist/src/public
 mv frontend/dist/* ./dist/src/public
 
-cp .env ./dist/src/.env
+cp .env ./dist/.env
+cp package.json ./dist/package.json
+cp yarn.lock ./dist/yarn.lock
+cp pm2.json ./dist/pm2.json
